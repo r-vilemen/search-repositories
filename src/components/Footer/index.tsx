90b05873b111ef41c69415f91d5257ee0f3f979f
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2022-09-06 17:41:27 -0300

    add footer and fix styles

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Container, CopyrightText, LinkToLinkedin } from "./styles";
+
+export const Footer = () => {
+  const currentYear = new Date();
+  const year = currentYear.getFullYear();
+  const LinkedinURL = "https://linkedin.com/in/rodrigo-vilemen/";
+
+  return (
+    <Container>
+      {/* <CopyrightText>
+        {`Copyrigth © ${year} - Rodrigo Vilemen - Todos os direitos reservados.`}
+      </CopyrightText> */}
+      <CopyrightText>Desenvolvido por:</CopyrightText>
+      <LinkToLinkedin onClick={() => window.open(LinkedinURL, "_blank")}>
+        Rodrigo Vilemen
+      </LinkToLinkedin>
+    </Container>
+  );
+};
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Footer } from "../../components/Footer";
 import {
   Container,
   MsgError,
@@ -60,6 +61,7 @@ export const Home = () => {
         </SearchBtn>
       </SearchGroup>
       {error && <MsgError>Usuário não encontrado</MsgError>}
+      <Footer />
     </Container>
   );
 };
